2534bfb8117d19df6d755b7c047bd0ff772beba1
cuntAdnan <adnanbarwatiya32@gmail.com>
2024-07-28 01:10:04 +0530

    second commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 import streamlit as st
 import cleantext
 
-
 st.header('Sentiment Analysis')
 with st.expander('Analyze Text'):
     text = st.text_input('Text here: ')
@@ -13,10 +12,7 @@ with st.expander('Analyze Text'):
         st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
 
 
-    pre = st.text_input('Clean Text: ')
-    if pre:
-        st.write(cleantext.clean(pre, clean_all= False, extra_spaces=True ,
-                                 stopwords=True ,lowercase=True ,numbers=True , punct=True))
+    
 
 with st.expander('Analyze CSV'):
     upl = st.file_uploader('Upload file')
